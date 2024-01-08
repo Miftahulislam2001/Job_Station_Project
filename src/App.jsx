7991d9638f931cc06fcaccca49777746d6ba716b
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-08 12:38:43 +0600

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='underline text-red-600'>Wellcome Project</h2>
+    </div>
+  );
+};
+
+export default App;
